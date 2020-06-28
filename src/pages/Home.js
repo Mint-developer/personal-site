f613beb57dc9d88a33952f1b753bea0a95e41b93
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2020-06-28 17:36:50 +0500

    Moved js files from public to scr

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Sidebar from '../components/Sidebar';
 import MainContent from '../components/MainContent';
 
 
-function Home() {
-	return (
-        <div>
-            <div className="art-app art-app-onepage">
-            
-                <div className="art-mobile-top-bar"></div>
 
-                <div className="art-app-wrapper">
+class Home extends Component {
 
-                    <div className="art-app-container">
-                        <Sidebar />
-                        <MainContent />
+
+    componentDidMount(){
+        //An array of assets
+        let scripts = [
+            { src: "assets/js/plugins/anime.min.js" },
+            { src: "assets/js/plugins/swiper.min.js" },
+            { src: "assets/js/plugins/progressbar.min.js" },
+            { src: "assets/js/plugins/smooth-scrollbar.min.js" },
+            { src: "assets/js/plugins/overscroll.min.js" },
+            { src: "assets/js/plugins/typing.min.js" },
+            { src: "assets/js/plugins/isotope.min.js" },
+            { src: "assets/js/plugins/fancybox.min.js" },
+            { src: "assets/js/plugins/swup.min.js" },
+            { src: "assets/js/main.js" },
+        ]
+        //Append the script element on each iteration
+        scripts.map(item => { 
+            const script = document.createElement("script")
+            script.src = item.src
+            script.async = true
+            document.body.appendChild(script)
+        })    
+     }
+
+    render(){
+        return (
+            <div>
+                <div className="art-app art-app-onepage">
+                    <div className="art-mobile-top-bar"></div>
+
+                    <div className="art-app-wrapper">
+                        <div className="art-app-container">
+                            <Sidebar />
+                            <MainContent />
+                        </div>
                     </div>
-
-                </div>
-
-                <div className="art-preloader">
-                   
-                    <div className="art-preloader-content">
-                        
-                        <h4>Muhammad Ijlal Nasir</h4>
-                       
-                        <div id="preloader" className="art-preloader-load"></div>
+    
+                    <div className="art-preloader">
+                        <div className="art-preloader-content">
+                            <h4>Muhammad Ijlal Nasir</h4>
+                            <div id="preloader" className="art-preloader-load"></div>
+                        </div>
                     </div>
-                   
                 </div>
+                <div id="swupMenu"></div>
             </div>
-            <div id="swupMenu"></div>
-        </div>
-
-        
-        
-	);
+        );
+    }
+	
 }
 
 export default Home;
