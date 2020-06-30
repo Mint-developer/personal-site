db07c81110b8f24fdc98cba8c181b78c76ab0869
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2020-06-30 17:10:24 +0500

    Updates

---
+++ precache-manifest.4b6f8d8067c86985f6b88e1c44af2eb4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fe20b47a2a58e43e4572f305c24bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641c173bb9500f281642",
+    "url": "/static/js/2.0dc2b689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0dc2b689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0b4be292db66ceded8",
+    "url": "/static/js/main.16790edf.chunk.js"
+  },
+  {
+    "revision": "ab7056e1160994051ac2",
+    "url": "/static/js/runtime-main.17f2f773.js"
+  },
+  {
+    "revision": "eb46d2f8d02d2ccf448aef97af574f16",
+    "url": "/static/media/bajwa.eb46d2f8.png"
+  },
+  {
+    "revision": "fec03229e4cd83eafdc03860115ec9b8",
+    "url": "/static/media/bg.fec03229.jpg"
+  },
+  {
+    "revision": "04e519d7286e6073c54902b26f542e10",
+    "url": "/static/media/bri.04e519d7.jpg"
+  },
+  {
+    "revision": "dd09a7c867ed6bb601b9fff1584fcb45",
+    "url": "/static/media/cv.dd09a7c8.pdf"
+  },
+  {
+    "revision": "48f31818379283388e1c203c57ea4176",
+    "url": "/static/media/face-1.48f31818.jpg"
+  },
+  {
+    "revision": "f5643104415a4c631939343607073621",
+    "url": "/static/media/face-2.f5643104.png"
+  },
+  {
+    "revision": "2fa182d21981b31465b18ced59a0a968",
+    "url": "/static/media/gulberg.2fa182d2.png"
+  },
+  {
+    "revision": "ec8f764f24dba7fceb0aea1b20415b60",
+    "url": "/static/media/jinn-bo.ec8f764f.png"
+  },
+  {
+    "revision": "b5252fd1dff556dd9ae285ccc41b42ca",
+    "url": "/static/media/jinn.b5252fd1.png"
+  }
+]);
