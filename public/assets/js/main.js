07cc74e5d0be9f6389c9b03bfdd70a9c003e87ba
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2020-06-30 12:10:25 +0500

    changed the progress bar values for langs

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -119,7 +119,7 @@ $(function() {
     }
   });
 
-  bar.animate(1);
+  bar.animate(0.9);
 
   var bar = new ProgressBar.Circle(circleprog2, {
     strokeWidth: 7,
@@ -137,7 +137,7 @@ $(function() {
     }
   });
 
-  bar.animate(0.9);
+  bar.animate(1);
 
   // var bar = new ProgressBar.Circle(circleprog3, {
   //   strokeWidth: 7,
